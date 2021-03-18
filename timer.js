521499af42d99a4b2c76fbc1c8d1a6b6004feb4d
Mubashir Chandle <61664272+mubashirchandle@users.noreply.github.com>
2021-03-18 19:35:46 +0530

    Fix bug: made the timer run only once to avoid recursion

--- timer.js
+++ timer.js
@@ -1,21 +1,30 @@
-var TIMER_DURATION = 60;
+// Multiply the required seconds by 10 to allow the timer to run in 100 ms intervals for better responsiveness in tab-change detection.
+var TIMER_DURATION = 60 * 10;
 
-function startTimer() {
+function startTimer(callback) {
     var timeLeft = TIMER_DURATION;
 
     var timer = setInterval(() => {
         if (timeLeft <= 0) {
             clearInterval(timer);
+            callback(true);
         }
 
-        document.getElementById("timer_text").innerHTML = timeLeft;
+        // As the time is multiplied by 10 for responsiveness, divide it by 10 again for accurate display in seconds.
+        document.getElementById("timer_text").innerHTML = Math.ceil(timeLeft / 10);
         timeLeft -= 1;
 
         if (!document.hasFocus()) {
-            startTimer();
-            timeLeft = 0;
+            clearInterval(timer);
+            callback(false);
         }
-    }, 1000);
+    }, 100);
 }
 
-startTimer();
+startTimer((timerCompleted) => {
+    if (timerCompleted) {
+        document.getElementById("status").innerHTML = "Timer Finished!";
+    } else {
+        document.getElementById("status").innerHTML = "Timer Interrupted!";
+    }
+});
